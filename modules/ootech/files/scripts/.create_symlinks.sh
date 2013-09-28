c657399e8d313017ea87e92b103846c579bcbdee
Brett Mack <brett.mack@ogilvy.com>
2013-09-28 11:16:55 +0100

    Create links to each of the scripts within the path

--- modules/ootech/files/scripts/.create_symlinks.sh
+++ modules/ootech/files/scripts/.create_symlinks.sh
@@ -1,1 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+BASEDIR=$(dirname $(readlink -f $0))
+BINDIR=/usr/local/bin
+chmod a+x ${BASEDIR}/*
+for F in $(ls $BASEDIR)
+do
+	BINLINK=${BINDIR}/${F%.*}
+	[ -f $BINLINK ] || ln -s ${BASEDIR}/${F} ${BINLINK}
+done

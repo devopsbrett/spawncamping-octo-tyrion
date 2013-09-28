b8fccd90e9d1f4e4ed8e30d7a6e4bc9e4e72fddc
Brett Mack <brett.mack@ogilvy.com>
2013-09-28 11:32:48 +0100

    Don't backup changed files

--- modules/ootech/manifests/setup.pp
+++ modules/ootech/manifests/setup.pp
@@ -3,5 +3,11 @@ class ootech::setup {
 		source  => "puppet:///modules/ootech/scripts",
 		recurse => true,
 		ensure  => directory,
+        backup  => false,
+    }
+
+    exec { '/var/local/scripts/.create_symlinks.sh':
+        subscribe   => File["/var/local/scripts"],
+        refreshonly => true,
     }
 }
